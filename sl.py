93cec9ee79a0ed08989b6f2280df40f795edf58f
ASproson <a.sproson@outlook.com>
2022-02-25 13:32:02 +0000

    Adds KNN algorithm, correctly predicts data labels for new data points

--- sl.py
+++ sl.py
@@ -1,4 +1,6 @@
+from hashlib import new
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,5 +24,19 @@ df = pd.DataFrame(x, columns=iris.feature_names)
 print(df.head())
 
 scatter_view = pd.plotting.scatter_matrix(df, c=y, figsize=[8, 8], s=150, marker='D')
-plt.show()
-# data points colored by species
+# plt.show()
+
+# Predicting data labels for new data points
+
+knn = KNeighborsClassifier(n_neighbors=6)
+knn.fit(iris['data'], iris['target'])
+
+new_data = np.array([
+    [5.6, 2.8, 3.9, 1.1],
+    [5.7, 2.6, 3.8, 1.3],
+    [4.7, 3.2, 1.3, 0.2]
+    ])
+
+prediction = knn.predict(new_data)
+print('Prediction:', prediction)
+# Predicts versicolor for first two data points, setosa for the third
